ac83fba70eb1f09e0479d08981ae479419e34d83
MOSAWIx <156970100+MOSAWIx@users.noreply.github.com>
2024-05-07 01:41:09 +0200

    Add files via upload

---
+++ ajouter.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajouter</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+       
+       if(isset($_POST['button'])){
+           
+           extract($_POST);
+           
+           if(isset($nom) && isset($prenom) && $age){
+
+                include_once "connexion.php";
+               
+                $req = mysqli_query($con , "INSERT INTO Employe VALUES(NULL, '$nom', '$prenom','$age')");
+                if($req){
+                    header("location: index.php");
+                }else {
+                    $message = "Employé non ajouté";
+                }
+
+           }else {
+               
+               $message = "Veuillez remplir tous les champs !";
+           }
+       }
+    
+    ?>
+    <div class="form">
+        <a href="index.php" class="back_btn"><img src="images/back.png"> Retour</a>
+        <h2>Ajouter un employé</h2>
+        <p class="erreur_message">
+            <?php 
+            
+            if(isset($message)){
+                echo $message;
+            }
+            ?>
+
+        </p>
+        <form action="" method="POST">
+            <label>Nom</label>
+            <input type="text" name="nom">
+            <label>Prénom</label>
+            <input type="text" name="prenom">
+            <label>âge</label>
+            <input type="number" name="age">
+            <input type="submit" value="Ajouter" name="button">
+        </form>
+    </div>
+</body>
+</html>
---
+++ supprimer.php
@@ -0,0 +1,10 @@
+<?php
+  
+  include_once "connexion.php";
+  
+  $id= $_GET['id'];
+  
+  $req = mysqli_query($con , "DELETE FROM employe WHERE id = $id");
+  
+  header("Location:index.php")
+?>
